57d25679dd810516e83b6ed7515f55414689140e
herbicus <sonofman_x@hotmail.com>
2015-02-04 23:07:30 -0500

    first commit

---
+++ app/frameworks/components/reddit/controller/reddit-controller.js
@@ -0,0 +1,10 @@
+angular.module('redditModule').controller('redditController', ['$scope', 'RedditFactory', function ($scope, RedditFactory) {
+  var ctrl = this;
+
+  ctrl.reddit = new RedditFactory();
+
+  ctrl.reddit.nextPage();
+
+
+
+}]);
---
+++ app/frameworks/components/reddit/service/reddit-service.js
@@ -0,0 +1,29 @@
+angular.module('redditModule').service('redditService', ['$http', '$q', function($http, $q) {
+
+  // assumes a node server running on port 9000
+  // export PORT=9000
+  //var SERVICE_URL_PREFIX = 'http://localhost:9000';
+
+  /**
+   * function getData
+   * service call pattern using angular $q promises
+   * resolve returns response, reject returns error
+   * @returns {promise.promise|jQuery.promise}
+   */
+  this.nextPage = function() {
+    var deferred = $q.defer();
+    var url = 'http://api.reddit.com/hot?after=' + this.after + ' in &jsonp=JSON_CALLBACK';
+
+    $http.jsonp(url)
+      .success(function (results) {
+        var data = results || [];
+        deferred.resolve(data);
+      })
+      .error(function (error) {
+        deferred.reject (error);
+      });
+
+    return deferred.promise;
+  };
+
+}]);
